502fa86e149a011f8f81680782422c4f58ad17c6
jdoherty61 <jdoherty61@qub.ac.uk>
2021-03-02 14:02:01 +0000

    add comment to the setAuthToken

--- frontend/src/utils/setAuthToken.js
+++ frontend/src/utils/setAuthToken.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 // when we have a token, we want to send it with every request - setting authetnication
 
+// ----------------------------------------------- REFERENCE(S) -----------------------------------------------
+// ***** TUTORIAL/COURSE THAT HELPED WITH THIS OVERALL PROCESS AND PARTICULAR FILE *****
+// Brad Traversy, 2019, MERN Stack Front To Back: Full Stack React, Redux & Node.js, https://www.udemy.com/share/101WIoAEYbcV9RRnUD/
+// Helped with understanding overall authentication.
+// -------------------------------------------------------------------------------------------------------------
+
 const setAuthToken = token => {
     if(token) {
         axios.defaults.headers.common['x-auth-token'] = token;
