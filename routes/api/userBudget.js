0b3f9268e6323b434ea546696057969d3055cd42
jdoherty61 <jdoherty61@qub.ac.uk>
2021-03-02 13:11:49 +0000

    another minor comment added

--- routes/api/userBudget.js
+++ routes/api/userBudget.js
@@ -14,6 +14,7 @@ const getUserBudgetUrl = "/me";
 
 // ----------------------------------------------- REFERENCE(S) -----------------------------------------------
 // ***** TUTORIAL/COURSE THAT HELPED WITH THIS OVERALL PROCESS AND PARTICULAR FILE *****
+// I used this tutorial for the basics of API development to implement my own functionality for features such as this user budget.
 // Brad Traversy, 2019, MERN Stack Front To Back: Full Stack React, Redux & Node.js, https://www.udemy.com/share/101WIoAEYbcV9RRnUD/
 // -------------------------------------------------------------------------------------------------------------
 
